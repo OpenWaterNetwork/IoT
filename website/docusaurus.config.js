9f6b17dabddda4c8fcf42086ae3966b68ecd710a
Fabian Astudillo <fabian.astudillo.salinas@gmail.com>
2021-04-20 22:35:50 -0500

    Ñ#	modificado:     website/docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,13 +77,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/OpenWaterNetwork/IoT/edit/master/website/',
+            'https://github.com/OpenWaterNetwork/IoT/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/OpenWaterNetwork/IoT/edit/master/website/blog/',
+            'https://github.com/OpenWaterNetwork/IoT/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
