24951c400a87aa65faee2a4dc66e3d50816e2654
Fabian Astudillo <fabian.astudillo.salinas@gmail.com>
2022-04-13 21:20:35 -0500

    Update folder structure

--- gateway/NanoGatewayLopy4/gatewayID.py
+++
@@ -1,8 +0,0 @@
-# Run this code on your Lopy4 device to get the LoRa device EUI that you need
-# to enter when registering the device on TTN
-from network import LoRa
-import ubinascii
-
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
-print("DevEUI: %s" % (ubinascii.hexlify(lora.mac()).decode('ascii')))
-
